79ef4955e554b5baf761bb31527729bea83da5af
Tapka3000 <circle.table@yandex.ru>
2023-10-28 18:43:42 +0300

    Commit

--- src/main/java/org/example/Cup.java
+++ src/main/java/org/example/Cup.java
@@ -1,2 +1,10 @@
-package org.example;public class Cup {
+package org.example;
+
+public class Cup {
+    public static void main(String[] args) {
+        int cup = 200;
+        double procent = 0.5;
+        double polovina = cup * procent;
+        System.out.println(polovina);
+    }
 }
