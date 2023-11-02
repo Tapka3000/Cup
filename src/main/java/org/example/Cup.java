31743a250406260f40064d39864a55b4679d9930
Tapka3000 <circle.table@yandex.ru>
2023-11-02 10:57:01 +0300

    Commit

--- src/main/java/org/example/Cup.java
+++ src/main/java/org/example/Cup.java
@@ -3,10 +3,10 @@ package org.example;
 public class Cup {
     public static void main(String[] args) {
         int cup = 200;
-        double procent = 0.5;
-        double polovina = cup * procent;
+        double percent = 0.5;
+        double half = cup * percent;
+
         System.out.println("Объем стакана " + cup + " мл");
-        System.out.println("Стакан заполнен на " + procent + " из 1");
-        System.out.println("Стакан заполнен на " + polovina + " мл");
+        System.out.println("Стакан заполнен на " + half + " мл");
     }
 }
